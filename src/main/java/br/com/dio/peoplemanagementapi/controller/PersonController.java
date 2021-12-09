e83bde0a63b2dda53494b7154ccfab164c0932b0
Samuel M. dos Santos <samuelmacedo2011@gmail.com>
2021-12-09 20:10:05 -0300

    Criação de Método Delete

--- src/main/java/br/com/dio/peoplemanagementapi/controller/PersonController.java
+++ src/main/java/br/com/dio/peoplemanagementapi/controller/PersonController.java
@@ -40,4 +40,12 @@ public class PersonController {
         return personService.findById(id);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletById(@PathVariable Long id) throws PersonNotFoundException{
+        personService.delete(id);
+
+    }
+
+
 }
